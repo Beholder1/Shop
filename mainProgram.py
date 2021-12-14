4c3be2ba6c55c86723bab5bfc61ac36e1a913a28
Kamil <kamil.limanowa@gmail.com>
2021-12-14 19:02:30 +0100

    13

--- mainProgram.py
+++ mainProgram.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from user import User
 from product import Product
 from widgets import AutocompleteCombobox
-from datetime import datetime
+
 
 
 class MainProgram:
@@ -156,51 +156,12 @@ class MainProgram:
         frame5.grid(row=0, column=1, sticky="nwse")
         frame5a = tk.Frame(frame5, bg=bgColor, borderwidth=1, relief=tk.RIDGE)
         frame5a.grid(row=0, column=0, sticky="nwse")
-        ttk.Label(frame5a, text="Pracownik: ").grid(row=0, column=0, sticky="w")
-        comboEmployees = AutocompleteCombobox(frame5a)
-        comboEmployees.set_completion_list(self.db.fetchColumnAll("users", "name"))
-        comboEmployees.grid(row=0, column=1, sticky="w")
-        comboEmployees.focus_set()
-        showEmployeeButton = tk.Button(frame5a, text="Pokaż",
-                                       command=lambda: self.show(frame5a, 1, 0, User(
-                                           self.db.fetch("users", "name", comboEmployees.get())[2])))
-        showEmployeeButton.grid(row=0, column=2)
+        user.showUser(frame5a, self.show)
 
         frame5b = tk.Frame(frame5, bg=bgColor, borderwidth=1, relief=tk.RIDGE)
         frame5b.grid(row=0, column=1, sticky="nwse")
+        user.addUser(frame5b)
 
-        ttk.Label(frame5b, text="Imię: ").grid(row=0, column=0, sticky="w")
-        entryEmployeeName = ttk.Entry(frame5b)
-        entryEmployeeName.grid(row=0, column=1, sticky="w")
-
-        ttk.Label(frame5b, text="Nazwisko: ").grid(row=1, column=0, sticky="w")
-        entryEmployeeLastName = ttk.Entry(frame5b)
-        entryEmployeeLastName.grid(row=1, column=1, sticky="w")
-
-        ttk.Label(frame5b, text="PESEL: ").grid(row=2, column=0, sticky="w")
-        entryEmployeePesel = ttk.Entry(frame5b)
-        entryEmployeePesel.grid(row=2, column=1, sticky="w")
-
-        ttk.Label(frame5b, text="Email: ").grid(row=3, column=0, sticky="w")
-        entryEmployeeEmail = ttk.Entry(frame5b)
-        entryEmployeeEmail.grid(row=3, column=1, sticky="w")
-
-        ttk.Label(frame5b, text="Pensja: ").grid(row=4, column=0, sticky="w")
-        entryEmployeeSalary = ttk.Entry(frame5b)
-        entryEmployeeSalary.grid(row=4, column=1, sticky="w")
-
-        addEmployeeButton = tk.Button(frame5, text="Dodaj",
-                                      command=lambda: self.db.insertUser(user.dept_id,
-                                                                         self.createLogin(entryEmployeeName.get(),
-                                                                                          entryEmployeeLastName.get()),
-                                                                         entryEmployeePesel.get(), "pracownik",
-                                                                         entryEmployeeEmail.get(),
-                                                                         entryEmployeeName.get(),
-                                                                         entryEmployeeLastName.get(),
-                                                                         entryEmployeeSalary.get(),
-                                                                         entryEmployeePesel.get(), datetime.now(),
-                                                                         user.id))
-        addEmployeeButton.grid(row=5, column=1)
 
         frame1 = tk.Frame(root, bg=bgColor, borderwidth=1, relief=tk.RIDGE)
         frame1.grid(row=0, column=1, sticky="nwse")
--- user.py
+++ user.py
@@ -1,9 +1,12 @@
 from db import Database
-
+import tkinter as tk
+from tkinter import ttk
+from widgets import AutocompleteCombobox
+from datetime import datetime
 
 class User:
     def __init__(self, login):
-        db = Database()
+        self.db = Database()
         self.login = login
         [self.id,
          self.dept_id,
@@ -16,7 +19,63 @@ class User:
          self.pesel,
          self.creationDate,
          self.lastLogin,
-         self.employerId] = [db.fetch("users", "login", login)[i] for i in (0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)]
+         self.employerId] = [self.db.fetch("users", "login", login)[i] for i in (0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)]
+
+    def showUser(self, frame, show):
+        ttk.Label(frame, text="Pracownik: ").grid(row=0, column=0, sticky="w")
+        comboEmployees = AutocompleteCombobox(frame)
+        comboEmployees.set_completion_list(self.db.fetchColumnAll("users", "name"))
+        comboEmployees.grid(row=0, column=1, sticky="w")
+        comboEmployees.focus_set()
+        showEmployeeButton = tk.Button(frame, text="Pokaż",
+                                       command=lambda: show(frame, 1, 0, User(
+                                           self.db.fetch("users", "name", comboEmployees.get())[2])))
+        showEmployeeButton.grid(row=0, column=2)
+
+    def addUser(self, frame):
+        ttk.Label(frame, text="Imię: ").grid(row=0, column=0, sticky="w")
+        entryEmployeeName = ttk.Entry(frame)
+        entryEmployeeName.grid(row=0, column=1, sticky="w")
+
+        ttk.Label(frame, text="Nazwisko: ").grid(row=1, column=0, sticky="w")
+        entryEmployeeLastName = ttk.Entry(frame)
+        entryEmployeeLastName.grid(row=1, column=1, sticky="w")
+
+        ttk.Label(frame, text="PESEL: ").grid(row=2, column=0, sticky="w")
+        entryEmployeePesel = ttk.Entry(frame)
+        entryEmployeePesel.grid(row=2, column=1, sticky="w")
+
+        ttk.Label(frame, text="Email: ").grid(row=3, column=0, sticky="w")
+        entryEmployeeEmail = ttk.Entry(frame)
+        entryEmployeeEmail.grid(row=3, column=1, sticky="w")
+
+        ttk.Label(frame, text="Pensja: ").grid(row=4, column=0, sticky="w")
+        entryEmployeeSalary = ttk.Entry(frame)
+        entryEmployeeSalary.grid(row=4, column=1, sticky="w")
+
+        addEmployeeButton = tk.Button(frame, text="Dodaj",
+                                      command=lambda: self.db.insertUser(self.dept_id,
+                                                                         self.createLogin(entryEmployeeName.get(),
+                                                                                          entryEmployeeLastName.get()),
+                                                                         entryEmployeePesel.get(), "pracownik",
+                                                                         entryEmployeeEmail.get(),
+                                                                         entryEmployeeName.get(),
+                                                                         entryEmployeeLastName.get(),
+                                                                         entryEmployeeSalary.get(),
+                                                                         entryEmployeePesel.get(), datetime.now(),
+                                                                         self.id))
+        addEmployeeButton.grid(row=5, column=1)
+
+    def createLogin(self, name, lastName):
+        if len(name) > 3:
+            login = name[:-(len(name) - 3)]
+        else:
+            login = name
+        if len(lastName) > 3:
+            login = login + lastName[:-(len(lastName) - 3)]
+        else:
+            login = login + lastName
+        return login
 
     def __str__(self):
         return "Login: " + str(self.login) + "\n" + \
